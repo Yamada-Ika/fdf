e3c96f4dd6e99622582c0d76dfed72f142570922
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-01-01 21:17:06 +0900

    [Move]

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 
 #define ESCAPE 0xff1b
+#define WIDTH 480
+#define HEIGHT 480
 
 typedef struct s_vars
 {
@@ -23,7 +25,7 @@ int	main(void)
 	vars.mlx = mlx_init();
 	if (vars.mlx == NULL)
 		return (0);
-	vars.win = mlx_new_window(vars.mlx, 640, 480, "Hello world!");
+	vars.win = mlx_new_window(vars.mlx, WIDTH, HEIGHT, "Hello world!");
 	mlx_key_hook(vars.win, key_hook, &vars);
 	mlx_loop(vars.mlx);
 }
