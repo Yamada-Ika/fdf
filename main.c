d4d62e4882124b09e6e2713427549a6687925703
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-01-01 21:14:35 +0900

    [Add]close if esc pushed

--- main.c
+++ main.c
@@ -1,44 +1,29 @@
 #include "mlx.h"
+#include <stdio.h>
 
-typedef struct s_data
+#define ESCAPE 0xff1b
+
+typedef struct s_vars
 {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}	t_data;
+	void	*mlx;
+	void	*win;
+}	t_vars;
 
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+int	key_hook(int keycode, t_vars *vars)
 {
-	char	*dst;
-
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int *)dst = color;
-}
-
-void	draw_rect(t_data *data, int width, int height)
-{
-	for (int y = 0; y < height; y++)
-	{
-		for (int x = 0; x < width; x++)
-		{
-			my_mlx_pixel_put(data, x, y, 0x00FF0000);
-		}
-	}
+	if (keycode == ESCAPE)
+		mlx_destroy_window(vars->mlx, vars->win);
+	return (0);
 }
 
 int	main(void)
 {
-	void	*mlx;
-	void	*mlx_win;
-	t_data	img;
+	t_vars	vars;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	img.img = mlx_new_image(mlx, 1920, 1080);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	draw_rect(&img, 100, 100);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
+	vars.mlx = mlx_init();
+	if (vars.mlx == NULL)
+		return (0);
+	vars.win = mlx_new_window(vars.mlx, 640, 480, "Hello world!");
+	mlx_key_hook(vars.win, key_hook, &vars);
+	mlx_loop(vars.mlx);
 }
