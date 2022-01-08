c223190e17e9b03083ef084e5d9716c3f01a9c2b
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-01-08 20:00:27 +0900

    [Fix]Work if map is big e.g. julia.fdf

--- main.c
+++ main.c
@@ -48,6 +48,8 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
 
+	if (x < 0 || y < 0)
+		return ;
 	dst = data->addr + (y * data->line_len + x * (data->bits_per_pixel / 8));
 	*(unsigned int*)dst = color;
 }
