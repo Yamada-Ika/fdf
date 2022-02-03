c9478a30a1db0005060779b98900862615b3fa30
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-02-03 22:05:01 +0900

    fix:

--- src/hook.c
+++ src/hook.c
@@ -6,34 +6,56 @@ void	ft_install_hook(t_vars *vars)
 	mlx_mouse_hook(vars->win, ft_mouse_hook, vars);
 }
 
-static bool	is_valid_keycode(int keycode)
+static bool	is_shift_key(int keycode)
 {
 	return ((keycode == RIGHT || keycode == LEFT
-			|| keycode == UP || keycode == DOWN
-			|| keycode == I || keycode == P
+			|| keycode == UP || keycode == DOWN));
+}
+
+static bool	is_switch_projection_key(int keycode)
+{
+	return ((keycode == I || keycode == P
 			|| keycode == C));
+}
+
+static bool	is_valid_key(int keycode)
+{
+	return (is_shift_key(keycode)
+			|| is_switch_projection_key(keycode));
+}
+
+void	ft_incre_or_decre_shift_var(int keycode, int *x, int *y)
+{
+	if (keycode == RIGHT)
+		*x += 5;
+	if (keycode == LEFT)
+		*x -= 5;
+	if (keycode == UP)
+		*y -= 5;
+	if (keycode == DOWN)
+		*y += 5;
 }
 
 int	ft_key_hook(int keycode, t_vars *vars)
 {
 	if (keycode == ESCAPE)
-		exit(0);
-	if (keycode == RIGHT)
-		vars->shift_x += 5;
-	if (keycode == LEFT)
-		vars->shift_x -= 5;
-	if (keycode == UP)
-		vars->shift_y -= 5;
-	if (keycode == DOWN)
-		vars->shift_y += 5;
+	{
+		mlx_destroy_image(vars->mlx, vars->img.img);
+		mlx_loop_end(vars->mlx);
+	}
+	if (is_shift_key(keycode))
+		ft_incre_or_decre_shift_var(keycode, &(vars->shift_x), &(vars->shift_y));
 	if (keycode == I)
 		ft_set_isometric(vars);
 	if (keycode == P)
 		ft_set_parallel(vars);
 	if (keycode == C)
 		ft_set_conic(vars);
-	if (is_valid_keycode(keycode))
+	if (is_valid_key(keycode))
+	{
+		mlx_destroy_image(vars->mlx, vars->img.img);
 		ft_display_map(vars);
+	}
 	return (0);
 }
 
@@ -47,6 +69,7 @@ int	ft_mouse_hook(int button, int x, int y, t_vars *vars)
 	if ((button == SCROLL_UP && vars->zoom_rate < 100)
 		|| (button == SCROLL_DOWN && vars->zoom_rate > 0.0))
 	{
+		mlx_destroy_image(vars->mlx, vars->img.img);
 		ft_display_map(vars);
 	}
 	return (0);
