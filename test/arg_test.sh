af0363885f41597e685709f84a153687956e4df0
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-02-04 01:01:25 +0900

    add: test case

--- test/arg_test.sh
+++ test/arg_test.sh
@@ -11,3 +11,9 @@ ${cur_path}/../fdf ${cur_path}/test_map/test4.fd
 echo $?
 ${cur_path}/../fdf ${cur_path}/test_map/test5.fdff
 echo $?
+${cur_path}/../fdf ${cur_path}/test_map/test6.fdff
+echo $?
+${cur_path}/../fdf ${cur_path}/test_map/test7.fdff
+echo $?
+${cur_path}/../fdf ${cur_path}/test_map/test8.fdff
+echo $?
