d2e7fc107b8672c3c5f8ae5bbf138565e74b19bc
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-02-04 01:23:03 +0900

    fix

--- src/display_map.c
+++ src/display_map.c
@@ -38,8 +38,8 @@ void	ft_set_z_color_helper0(t_2dcord *cord, t_vars *vars, int y, int x)
 
 void	ft_display_map_helper(t_data *img, t_vars *vars)
 {
-	int	y;
-	int	x;
+	int			y;
+	int			x;
 	t_2dcord	*cord;
 
 	y = 0;
@@ -78,7 +78,8 @@ void	ft_display_map(t_vars *vars)
 
 	mlx_clear_window(vars->mlx, vars->win);
 	img.img = mlx_new_image(vars->mlx, WIDTH, HEIGHT);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_len, &img.endian);
+	img.addr = mlx_get_data_addr(img.img,
+			&img.bits_per_pixel, &img.line_len, &img.endian);
 	ft_set_matrix(vars);
 	ft_display_map_helper(&img, vars);
 	mlx_put_image_to_window(vars->mlx, vars->win, img.img, 0, 0);
