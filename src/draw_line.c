c91872f8af7bba002e8417fd423b2491242502b0
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-02-04 01:17:54 +0900

    fix: adjust norm

--- src/draw_line.c
+++ src/draw_line.c
@@ -1,12 +1,21 @@
 #include "fdf.h"
 
+int	get_color_gradation(int color_code0, int color_code1, double step)
+{
+	if (color_code0 == 0)
+		color_code0 = 0xFFFFFF;
+	if (color_code1 == 0)
+		color_code1 = 0xFFFFFF;
+	return ((color_code1 - color_code0) / step);
+}
+
 void	ft_draw_line(t_data *img, t_2dcord *cord, t_vars *vars)
 {
 	double	delta_x;
 	double	delta_y;
 	int		step;
 	double	step_max;
-	int		color_step;
+	int		color_gradation;
 
 	ft_trans_cord(cord, vars);
 	delta_x = cord->x1 - cord->x0;
@@ -14,18 +23,14 @@ void	ft_draw_line(t_data *img, t_2dcord *cord, t_vars *vars)
 	step_max = ft_max(ft_abs(delta_x), ft_abs(delta_y));
 	delta_x /= step_max;
 	delta_y /= step_max;
-	if (vars->color0 == 0)
-		vars->color0 = 0xFFFFFF;
-	if (vars->color1 == 0)
-		vars->color1 = 0xFFFFFF;
-	color_step = (vars->color1 - vars->color0) / step_max;
+	color_gradation = get_color_gradation(vars->color0, vars->color1, step_max);
 	step = 0;
 	while (step < step_max)
 	{
 		cord->x0 += delta_x;
 		cord->y0 += delta_y;
 		my_mlx_pixel_put(img, cord->x0, cord->y0, vars->color0);
-		vars->color0 += color_step;
+		vars->color0 += color_gradation;
 		step++;
 	}
 }
