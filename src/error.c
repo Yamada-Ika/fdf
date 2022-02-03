c9ffb4eaa920bce89fc83fc488d50668bdfc4674
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-02-04 01:02:53 +0900

    add: error func

---
+++ src/error.c
@@ -0,0 +1,26 @@
+#include "fdf.h"
+
+void	*ft_print_error(char *message)
+{
+	perror(message);
+	return (NULL);
+}
+
+void	*ft_do_read_error_routine(void *p1, void *p2, void *p3, void *p4)
+{
+	all_free(p1, p2, p3, p4);
+	return (ft_print_error("Cannot read file"));
+}
+
+void	*ft_do_malloc_error_routine(void *p1, void *p2, void *p3, void *p4)
+{
+	all_free(p1, p2, p3, p4);
+	return (ft_print_error("Cannot allocate memmory"));
+}
+
+void	*ft_do_malloc_strs_error_routine(void **p1, void ***p2)
+{
+	free_strs(p1);
+	free_tristrs(p2);
+	return (ft_print_error("Cannot allocate memmory"));
+}
