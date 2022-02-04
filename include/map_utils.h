c04834c2a45f8d2a0c3ca56d5f971e7c22e7293c
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-02-04 19:16:49 +0900

    rename

--- include/map_utils.h
+++ include/map_utils.h
@@ -6,7 +6,7 @@
 /*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/02 15:44:19 by iyamada           #+#    #+#             */
-/*   Updated: 2022/01/07 18:14:48 by iyamada          ###   ########.fr       */
+/*   Updated: 2022/02/04 19:14:46 by iyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 int	ft_get_line_size(char **s);
 int	ft_get_map_column_count(char ***map);
-int	ft_get_map_raw_count(char ***map);
+int	ft_get_map_row_count(char ***map);
 int	ft_get_map_z_max(char ***map);
 
 #endif
