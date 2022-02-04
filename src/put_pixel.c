c82aa4bf254b94737708b948d634c36988b560f5
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-02-05 00:03:47 +0900

    fix

--- src/put_pixel.c
+++ src/put_pixel.c
@@ -4,7 +4,7 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
 
-	if (x < 0 || y < 0)
+	if (x < 0 || WIDTH - 1 < x || y < 0 || HEIGHT - 1 < y)
 		return ;
 	dst = data->addr + (y * data->line_len + x * (data->bits_per_pixel / 8));
 	*(unsigned int *)dst = color;
