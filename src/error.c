caecf023ee42af36329d68f4bca2b8083786d46e
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-02-06 22:11:01 +0900

    fix: type

--- src/error.c
+++ src/error.c
@@ -6,21 +6,21 @@ void	*ft_print_error(char *message)
 	return (NULL);
 }
 
-void	*ft_do_read_error_routine(void *p1, void *p2, void *p3, void *p4)
+void	*ft_do_read_error_routine(char *p1, char *p2, char *p3, char *p4)
 {
 	all_free(p1, p2, p3, p4);
 	return (ft_print_error("Cannot read file"));
 }
 
-void	*ft_do_malloc_error_routine(void *p1, void *p2, void *p3, void *p4)
+void	*ft_do_malloc_error_routine(char *p1, char *p2, char *p3, char *p4)
 {
 	all_free(p1, p2, p3, p4);
 	return (ft_print_error("Cannot allocate memmory"));
 }
 
-void	*ft_do_malloc_strs_error_routine(void **p1, void ***p2)
+void	*ft_do_malloc_strs_error_routine(char **p1, char ***p2)
 {
-	free_strs((char **)p1);
-	free_char_map((char ***)p2);
+	free_strs(p1);
+	free_char_map(p2);
 	return (ft_print_error("Cannot allocate memmory"));
 }
