f2ccb208620d1469d026ed6d31be833d866ce3d9
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-02-06 22:09:41 +0900

    split

--- src/display_map.c
+++ src/display_map.c
@@ -1,101 +1,34 @@
 #include "fdf.h"
 
-static void	_update_map(t_map **dst, t_map **src, size_t row_size, size_t column_size)
+static void	_set_map_origin(t_map_info *map)
 {
-	size_t	i;
-	size_t	j;
-
-	i = 0;
-	while (i < row_size)
-	{
-		j = 0;
-		while (j < column_size)
-		{
-			dst[i][j].x = src[i][j].x;
-			dst[i][j].y = src[i][j].y;
-			dst[i][j].z = src[i][j].z;
-			j++;
-		}
-		i++;
-	}
+	map->origin_x = (map->points[0][map->column_size - 1].x
+			- map->points[0][0].x) / 2 + map->points[0][0].x;
+	map->origin_y = (map->points[map->row_size - 1][0].y
+			- map->points[0][0].y) / 2 + map->points[0][0].y;
 }
 
-void	ft_display_map_helper(t_data *img, t_vars *vars)
+static void	_set_image_info(t_image_info *img, t_map_info *map)
 {
-	int		y;
-	int		x;
-	t_map	map0;
-	t_map	map1;
-
-	y = 0;
-	while (y < vars->map_row_size)
-	{
-		x = 0;
-		while (true)
-		{
-			if (x + 1 < vars->map_column_size)
-			{
-				map0.x = vars->map[y][x].x;
-				map0.y = vars->map[y][x].y;
-				map0.z = vars->map[y][x].z;
-				map0.color = vars->map[y][x].color;
-				map1.x = vars->map[y][x + 1].x;
-				map1.y = vars->map[y][x + 1].y;
-				map1.z = vars->map[y][x + 1].z;
-				map1.color = vars->map[y][x + 1].color;
-				ft_draw_line(img, &map0, &map1, vars->affine_matrix);
-				vars->map_tmp[y][x].x = map0.x;
-				vars->map_tmp[y][x].y = map0.y;
-				vars->map_tmp[y][x].z = map0.z;
-				vars->map_tmp[y][x].color = map0.color;
-				vars->map_tmp[y][x + 1].x = map1.x;
-				vars->map_tmp[y][x + 1].y = map1.y;
-				vars->map_tmp[y][x + 1].z = map1.z;
-				vars->map_tmp[y][x + 1].color = map1.color;
-			}
-			if (y + 1 < vars->map_row_size)
-			{
-				map0.x = vars->map[y][x].x;
-				map0.y = vars->map[y][x].y;
-				map0.z = vars->map[y][x].z;
-				map0.color = vars->map[y][x].color;
-				map1.x = vars->map[y + 1][x].x;
-				map1.y = vars->map[y + 1][x].y;
-				map1.z = vars->map[y + 1][x].z;
-				map1.color = vars->map[y + 1][x].color;
-				ft_draw_line(img, &map0, &map1, vars->affine_matrix);
-				vars->map_tmp[y][x].x = map0.x;
-				vars->map_tmp[y][x].y = map0.y;
-				vars->map_tmp[y][x].z = map0.z;
-				vars->map_tmp[y][x].color = map0.color;
-				vars->map_tmp[y + 1][x].x = map1.x;
-				vars->map_tmp[y + 1][x].y = map1.y;
-				vars->map_tmp[y + 1][x].z = map1.z;
-				vars->map_tmp[y + 1][x].color = map1.color;
-			}
-			if (x + 1 == vars->map_column_size)
-				break ;
-			x++;
-		}
-		y++;
-	}
-	_update_map(vars->map, vars->map_tmp, vars->map_row_size, vars->map_column_size);
-	// debug_map(vars->map, vars->map_row_size, vars->map_column_size);
+	img->img = mlx_new_image(map->mlx, WIDTH, HEIGHT);
+	img->addr = mlx_get_data_addr(img->img,
+			&img->bits_per_pixel, &img->line_len, &img->endian);
 }
 
-void	ft_display_map(t_vars *vars)
+void	ft_display_map(t_map_info *map)
 {
-	t_data	img;
+	t_image_info	img;
 
-	img.img = mlx_new_image(vars->mlx, WIDTH, HEIGHT);
-	img.addr = mlx_get_data_addr(img.img,
-			&img.bits_per_pixel, &img.line_len, &img.endian);
-	ft_calc_affine_matrix(vars);
-	ft_display_map_helper(&img, vars);
-	mlx_put_image_to_window(vars->mlx, vars->win, img.img, 0, 0);
-	mlx_string_put(vars->mlx, vars->win, 10, 650, 0xFFFFFF, "shift : arrow key");
-	mlx_string_put(vars->mlx, vars->win, 130, 650, 0xFFFFFF, "zoom : track ball");
-	mlx_string_put(vars->mlx, vars->win, 250, 650, 0xFFFFFF, "rotate : x y z key");
-	mlx_string_put(vars->mlx, vars->win, 380, 650, 0xFFFFFF, "projection : i isometrix p parallel c conic");
-	vars->img = img;
+	_set_image_info(&img, map);
+	map->img = img;
+	_set_map_origin(map);
+	ft_calc_affine_matrix(map);
+	ft_put_map_to_image(&img, map);
+	mlx_put_image_to_window(map->mlx, map->win, img.img, 0, 0);
+	mlx_string_put(map->mlx, map->win, 10, 650, 0xFFFFFF, "shift : arrow key");
+	mlx_string_put(map->mlx, map->win, 130, 650, 0xFFFFFF, "zoom : track ball");
+	mlx_string_put(map->mlx, map->win, 250, 650, 0xFFFFFF,
+		"rotate : x y z key");
+	mlx_string_put(map->mlx, map->win, 380, 650, 0xFFFFFF,
+		"projection : i isometrix p parallel c conic");
 }
---
+++ src/put_map_to_image.c
@@ -0,0 +1,75 @@
+#include "fdf.h"
+
+static void	_update_map_points(t_point **dst, t_point **src, size_t row_size, size_t col_size)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	while (i < row_size)
+	{
+		j = 0;
+		while (j < col_size)
+		{
+			dst[i][j].x = src[i][j].x;
+			dst[i][j].y = src[i][j].y;
+			dst[i][j].z = src[i][j].z;
+			dst[i][j].color = src[i][j].color;
+			j++;
+		}
+		i++;
+	}
+}
+
+static void	_set_tmp_points_for_draw(t_point *tmp_points, t_point **points, size_t i, size_t j)
+{
+	tmp_points->x = points[i][j].x;
+	tmp_points->y = points[i][j].y;
+	tmp_points->z = points[i][j].z;
+	tmp_points->color = points[i][j].color;
+}
+
+static void	_set_tmp_map_for_update(t_point **points_tmp_for_update, size_t i, size_t j, t_point *tmp_points)
+{
+	points_tmp_for_update[i][j].x = tmp_points->x;
+	points_tmp_for_update[i][j].y = tmp_points->y;
+	points_tmp_for_update[i][j].z = tmp_points->z;
+	points_tmp_for_update[i][j].color = tmp_points->color;
+}
+
+void	ft_put_map_to_image(t_image_info *img, t_map_info *map)
+{
+	size_t	i;
+	size_t	j;
+	t_point	tmp_for_draw[2];
+
+	i = 0;
+	while (i < map->row_size)
+	{
+		j = 0;
+		while (true)
+		{
+			if (j + 1 < map->column_size)
+			{
+				_set_tmp_points_for_draw(&(tmp_for_draw[0]), map->points, i, j);
+				_set_tmp_points_for_draw(&(tmp_for_draw[1]), map->points, i, j + 1);
+				ft_draw_line(img, &tmp_for_draw[0], &tmp_for_draw[1], map->mtx.affine);
+				_set_tmp_map_for_update(map->tmp_for_update, i, j, &(tmp_for_draw[0]));
+				_set_tmp_map_for_update(map->tmp_for_update, i, j + 1, &(tmp_for_draw[1]));
+			}
+			if (i + 1 < map->row_size)
+			{
+				_set_tmp_points_for_draw(&(tmp_for_draw[0]), map->points, i, j);
+				_set_tmp_points_for_draw(&(tmp_for_draw[1]), map->points, i + 1, j);
+				ft_draw_line(img, &tmp_for_draw[0], &tmp_for_draw[1], map->mtx.affine);
+				_set_tmp_map_for_update(map->tmp_for_update, i, j, &(tmp_for_draw[0]));
+				_set_tmp_map_for_update(map->tmp_for_update, i + 1, j, &(tmp_for_draw[1]));
+			}
+			if (j + 1 == map->column_size)
+				break ;
+			j++;
+		}
+		i++;
+	}
+	_update_map_points(map->points, map->tmp_for_update, map->row_size, map->column_size);
+}
