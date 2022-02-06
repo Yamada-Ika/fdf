eb645ca0756e2af87f52234f3082fadeed510ed1
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-02-07 04:13:16 +0900

    fix: type

--- src/init_struct.c
+++ src/init_struct.c
@@ -2,12 +2,12 @@
 
 void	init_map_info(t_map_info *map)
 {
-	map->yaw = 0.0;
-	map->roll = 0.0;
-	map->pitch = 0.0;
+	map->yaw = degree_to_radian(0);
+	map->roll = degree_to_radian(0);
+	map->pitch = degree_to_radian(0);
 	map->zoom_rate = 1.0;
-	map->shift_x = 0;
-	map->shift_y = 0;
+	map->shift_x = 0.0;
+	map->shift_y = 0.0;
 	map->mouse_x = 0.0;
 	map->mouse_y = 0.0;
 	ft_new_matrix(map);
