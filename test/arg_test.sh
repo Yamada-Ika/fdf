4533f0d80bba2202009fc9a64e449831da9f850e
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-02-07 01:39:55 +0900

    add: test

--- test/arg_test.sh
+++ test/arg_test.sh
@@ -2,18 +2,22 @@
 
 cur_path=$(pwd)
 ${cur_path}/../fdf ${cur_path}/test_map/test1.txt
-echo $?
 ${cur_path}/../fdf ${cur_path}/test_map/test2.file
-echo $?
 ${cur_path}/../fdf ${cur_path}/test_map/test3
-echo $?
 ${cur_path}/../fdf ${cur_path}/test_map/test4.fd
-echo $?
 ${cur_path}/../fdf ${cur_path}/test_map/test5.fdff
-echo $?
 ${cur_path}/../fdf ${cur_path}/test_map/test6.fdff
-echo $?
-${cur_path}/../fdf ${cur_path}/test_map/test7.fdff
-echo $?
-${cur_path}/../fdf ${cur_path}/test_map/test8.fdff
-echo $?
+${cur_path}/../fdf ${cur_path}/test_map/test7.fdf
+${cur_path}/../fdf ${cur_path}/test_map/test8.fdf
+${cur_path}/../fdf ${cur_path}/test_map/test9.fdf
+${cur_path}/../fdf ${cur_path}/test_map/test10.fdf
+${cur_path}/../fdf ${cur_path}/test_map/test11.fdf
+${cur_path}/../fdf ${cur_path}/test_map/test12.fdf
+${cur_path}/../fdf ${cur_path}/test_map/test13.fdf
+${cur_path}/../fdf ${cur_path}/test_map/test14.fdf
+${cur_path}/../fdf ${cur_path}/test_map/test15.fdf
+${cur_path}/../fdf ${cur_path}/test_map/test16.fdf
+${cur_path}/../fdf ${cur_path}/test_map/test17.fdf
+${cur_path}/../fdf ${cur_path}/test_map/test18.fdf
+${cur_path}/../fdf ${cur_path}/test_map/test19.fdf
+${cur_path}/../fdf ${cur_path}/test_map/test20.fdf
