f5eb9d83c4add69c8ecbcdf4d288dbf8e9b30f51
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-02-07 01:40:22 +0900

    delete: func

--- src/get_map_size.c
+++ src/get_map_size.c
@@ -1,15 +1,5 @@
 #include "stddef.h"
 
-size_t	ft_get_line_size(char **s)
-{
-	size_t	size;
-
-	size = 0;
-	while (s[size] != NULL)
-		size++;
-	return (size);
-}
-
 size_t	get_map_column_size(char ***map)
 {
 	size_t	size;
