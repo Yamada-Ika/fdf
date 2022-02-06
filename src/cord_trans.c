f0dcd0536ee4ecb0bd20ee76a9280321056f421a
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-02-06 22:10:23 +0900

    add: transform z val

--- src/cord_trans.c
+++ src/cord_trans.c
@@ -17,9 +17,13 @@ void	ft_rotation(double **matrix, double *x, double *y, double *z)
 			+ matrix[1][1] * prev_y
 			+ matrix[1][2] * prev_z
 			+ matrix[1][3]);
+	*z = (matrix[2][0] * prev_x
+			+ matrix[2][1] * prev_y
+			+ matrix[2][2] * prev_z
+			+ matrix[2][3]);
 }
 
-void	ft_trans_cord(t_map *map0, t_map *map1, double **matrix)
+void	ft_trans_cord(t_point *map0, t_point *map1, double **matrix)
 {
 	ft_rotation(matrix, &(map0->x), &(map0->y), &(map0->z));
 	ft_rotation(matrix, &(map1->x), &(map1->y), &(map1->z));
