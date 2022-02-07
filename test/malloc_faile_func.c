a6e149d97d5437f3d863fc0e43bcbe07999094e9
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-02-08 01:53:13 +0900

    fix: use srand

--- test/malloc_faile_func.c
+++ test/malloc_faile_func.c
@@ -4,11 +4,14 @@
 #include <stdlib.h>
 #include "../libft/libft.h"
 #include "../minilibx-linux/mlx.h"
+#include <time.h>
+#include <stdio.h>
 
-#define RANDOM_PARAM RAND_MAX
+#define RANDOM_PARAM 4
 
 char	**my_ft_split(char *s, char c)
 {
+	srand((unsigned int)time(NULL));
 	if (rand() % RANDOM_PARAM == 0)
 		return (NULL);
 	return (ft_split(s, c));
@@ -16,6 +19,7 @@ char	**my_ft_split(char *s, char c)
 
 void	*my_ft_calloc(size_t n, size_t size)
 {
+	srand((unsigned int)time(NULL));
 	if (rand() % RANDOM_PARAM == 0)
 		return (NULL);
 	return (ft_calloc(n, size));
@@ -23,6 +27,7 @@ void	*my_ft_calloc(size_t n, size_t size)
 
 void	*my_mlx_init(void)
 {
+	srand((unsigned int)time(NULL));
 	if (rand() % RANDOM_PARAM == 0)
 		return (NULL);
 	return (mlx_init());
@@ -30,6 +35,7 @@ void	*my_mlx_init(void)
 
 void	*my_mlx_new_window(void *xvar,int size_x,int size_y,char *title)
 {
+	srand((unsigned int)time(NULL));
 	if (rand() % RANDOM_PARAM == 0)
 		return (NULL);
 	return (mlx_new_window(xvar, size_x, size_x, title));
@@ -37,6 +43,7 @@ void	*my_mlx_new_window(void *xvar,int size_x,int size_y,char *title)
 
 void	*my_mlx_new_image(void *xvar,int width, int height)
 {
+	srand((unsigned int)time(NULL));
 	if (rand() % RANDOM_PARAM == 0)
 		return (NULL);
 	return (my_mlx_new_image(xvar, width, height));
