78c5be9cd052c0738fa0fa31548b022394897d5f
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-02-08 02:55:28 +0900

    refact

--- src/can_redisplay_map.c
+++ src/can_redisplay_map.c
@@ -4,8 +4,8 @@ bool	can_redisplay_map(t_map_info *map)
 {
 	mlx_destroy_image(map->mlx, map->img.img);
 	mlx_clear_window(map->mlx, map->win);
-	if (!can_display_map(map))
-		return (false);
-	else
+	if (can_display_map(map))
 		return (true);
+	else
+		return (false);
 }
