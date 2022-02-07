81990aea84929fef702df9eea244ae0b687d3b51
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-02-08 03:14:29 +0900

    add: print error

--- src/main.c
+++ src/main.c
@@ -16,18 +16,18 @@ int	main(int argc, char *argv[])
 	if (is_help_option(argc, argv))
 		return (print_usage());
 	if (is_invalid_args(argc, argv))
-		return (1);
+		return (print_error("Error: Invalid argument!"));
 	if (!can_read_map(argv[1], &map))
-		return (1);
+		return (print_error("Error: Cannot read map!"));
 	if (!can_init_map_info(&map))
-		return (1);
+		return (print_error("Error: Cannot initialize map!"));
 	if (!can_init_mlx(&map))
-		return (1);
+		return (print_error("Error: Cannot initialize minilibx variables!"));
 	if (!can_display_default_projection(&map))
 	{
 		delete_mlx(&map);
 		delete_map(&map);
-		return (1);
+		return (print_error("Error: Cannot display map!"));
 	}
 	install_hook(&map);
 	mlx_loop(map.mlx);
