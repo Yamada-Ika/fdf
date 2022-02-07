7bacea44cb0418040960c9601116fdcec6529db3
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-02-08 01:11:42 +0900

    add: NULL check

--- src/display_default_projection.c
+++ src/display_default_projection.c
@@ -1,19 +1,19 @@
 #include "fdf.h"
 
-void	display_default_projection(t_map_info *map)
+bool	can_display_default_projection(t_map_info *map)
 {
 	map->zoom_rate = 255 * pow(map->row_size * map->col_size, -0.507);
 	map->shift_x = WIDTH / 3;
 	map->shift_y = HEIGHT / 3;
 	map->mouse_x = WIDTH / 3;
 	map->mouse_y = HEIGHT / 3;
-	display_map(map);
-	mlx_destroy_image(map->mlx, map->img.img);
-	mlx_clear_window(map->mlx, map->win);
+	if (!can_display_map(map))
+		return (false);
 	map->yaw = degree_to_radian(-45);
-	display_map(map);
-	mlx_destroy_image(map->mlx, map->img.img);
-	mlx_clear_window(map->mlx, map->win);
+	if (!can_redisplay_map(map))
+		return (false);
 	map->roll = degree_to_radian(50);
-	display_map(map);
+	if (!can_redisplay_map(map))
+		return (false);
+	return (true);
 }
