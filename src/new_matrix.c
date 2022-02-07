3e824c7d000f5cc4cb91322a9753d96299dbfb3d
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-02-07 13:38:26 +0900

    add: NULL check

--- src/new_matrix.c
+++ src/new_matrix.c
@@ -5,15 +5,56 @@ static double	**_new_matrix(void)
 	double	**matrix;
 
 	matrix = (double **)ft_calloc(4, sizeof(double *));
+	if (matrix == NULL)
+		return (NULL);
 	matrix[0] = (double *)ft_calloc(4, sizeof(double));
+	if (matrix[0] == NULL)
+		return (matrix);
 	matrix[1] = (double *)ft_calloc(4, sizeof(double));
+	if (matrix[1] == NULL)
+		return (matrix);
 	matrix[2] = (double *)ft_calloc(4, sizeof(double));
+	if (matrix[2] == NULL)
+		return (matrix);
 	matrix[3] = (double *)ft_calloc(4, sizeof(double));
+	if (matrix[3] == NULL)
+		return (matrix);
 	set_unit_matrix(matrix);
 	return (matrix);
 }
 
-void	ft_new_matrix(t_map_info *map)
+static bool	is_null_in_matrix(double **matrix)
+{
+	if (matrix == NULL)
+		return (true);
+	if (matrix[0] == NULL
+		|| matrix[1] == NULL
+		|| matrix[2] == NULL
+		|| matrix[3] == NULL)
+		return (true);
+	else
+		return (false);
+}
+
+static	bool	_is_invalid_matrix_in_t_matrix(t_matrix *mtx)
+{
+	if (is_null_in_matrix(mtx->affine)
+		|| is_null_in_matrix(mtx->shift)
+		|| is_null_in_matrix(mtx->zoom)
+		|| is_null_in_matrix(mtx->move_cursor)
+		|| is_null_in_matrix(mtx->to_corner_for_zoom)
+		|| is_null_in_matrix(mtx->to_corner_for_rotate)
+		|| is_null_in_matrix(mtx->rotate_x)
+		|| is_null_in_matrix(mtx->rotate_y)
+		|| is_null_in_matrix(mtx->rotate_z)
+		|| is_null_in_matrix(mtx->move_map_origin)
+		|| is_null_in_matrix(mtx->tmp_mtx))
+		return (true);
+	else
+		return (false);
+}
+
+bool	can_new_matrix(t_map_info *map)
 {
 	t_matrix	matrix;
 
@@ -29,4 +70,7 @@ void	ft_new_matrix(t_map_info *map)
 	matrix.shift = _new_matrix();
 	matrix.affine = _new_matrix();
 	map->mtx = matrix;
+	if (_is_invalid_matrix_in_t_matrix(&(map->mtx)))
+		return (false);
+	return (true);
 }
