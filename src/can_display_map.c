f2851870dd4b9f169fc070645e34b1a2f7dec73d
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-02-08 01:09:56 +0900

    rename

--- src/can_display_map.c
+++ src/can_display_map.c
@@ -8,14 +8,20 @@ static void	_set_map_origin(t_map_info *map)
 			- map->points[0][0].y) / 2 + map->points[0][0].y;
 }
 
-static void	_set_image_info(t_image_info *img, t_map_info *map)
+static bool	_can_set_image_info(t_image_info *img, t_map_info *map)
 {
 	img->img = mlx_new_image(map->mlx, WIDTH, HEIGHT);
+	if (img->img == NULL)
+	{
+		return (false);
+	}
 	img->addr = mlx_get_data_addr(img->img,
 			&img->bits_per_pixel, &img->line_len, &img->endian);
+	map->img = *img;
+	return (true);
 }
 
-void	init_map_meta_info(t_map_info *map)
+static void	_init_map_meta_info(t_map_info *map)
 {
 	map->yaw = degree_to_radian(0);
 	map->roll = degree_to_radian(0);
@@ -27,7 +33,7 @@ void	init_map_meta_info(t_map_info *map)
 	map->mouse_y = 0.0;
 }
 
-void	display_usage(t_map_info *map)
+static void	_display_usage(t_map_info *map)
 {
 	mlx_string_put(map->mlx, map->win, 100, 650, 0xFFFFFF, "shift : arrow key");
 	mlx_string_put(map->mlx, map->win, 300, 650, 0xFFFFFF, "zoom : track ball");
@@ -35,18 +41,19 @@ void	display_usage(t_map_info *map)
 		"rotate : x y z key");
 }
 
-void	display_map(t_map_info *map)
+bool	can_display_map(t_map_info *map)
 {
 	t_image_info	img;
 
-	_set_image_info(&img, map);
+	if (!_can_set_image_info(&img, map))
+		return (false);
 	_set_map_origin(map);
-	map->img = img;
 	calc_affine_matrix(map);
 	put_map_to_image(map);
 	mlx_put_image_to_window(map->mlx, map->win, img.img, 0, 0);
 	update_map_points(map->points, map->for_update,
 		map->row_size, map->col_size);
-	display_usage(map);
-	init_map_meta_info(map);
+	_display_usage(map);
+	_init_map_meta_info(map);
+	return (true);
 }
