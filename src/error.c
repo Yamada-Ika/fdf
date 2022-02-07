74df7275a53d136c9003aec26d441668f71024a0
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-02-08 02:55:42 +0900

    fix: do not use perror

--- src/error.c
+++ src/error.c
@@ -9,18 +9,18 @@ void	*ft_print_error(char *message)
 void	*ft_do_read_error_routine(char *p1, char *p2, char *p3, char *p4)
 {
 	all_free(p1, p2, p3, p4);
-	return (ft_print_error("Cannot read file"));
+	return (NULL);
 }
 
 void	*ft_do_malloc_error_routine(char *p1, char *p2, char *p3, char *p4)
 {
 	all_free(p1, p2, p3, p4);
-	return (ft_print_error("Cannot allocate memmory"));
+	return (NULL);
 }
 
 void	*ft_do_malloc_strs_error_routine(char **p1, char ***p2)
 {
 	free_strs(p1);
 	free_char_map(p2);
-	return (ft_print_error("Cannot allocate memmory"));
+	return (NULL);
 }
