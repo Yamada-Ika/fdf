cceb9e26e04a73a5727ef9f29c5318fd667b0d4b
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-02-08 15:57:10 +0900

    rename

--- src/init_map_info.c
+++ src/init_map_info.c
@@ -5,10 +5,16 @@ bool	can_init_map_info(t_map_info *map)
 	map->yaw = degree_to_radian(0);
 	map->roll = degree_to_radian(0);
 	map->pitch = degree_to_radian(0);
-	map->zoom_rate = 1;
+	map->zoom_rate = 1.0;
 	map->shift_x = 0.0;
 	map->shift_y = 0.0;
 	map->mouse_x = 0.0;
 	map->mouse_y = 0.0;
-	return (can_new_matrix(map));
+	if (can_new_matrix(map))
+		return (true);
+	else
+	{
+		delete_map(map);
+		return (false);
+	}
 }
