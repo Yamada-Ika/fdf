7673bf5fce4262acfafd2a67b1a64a9f5121f253
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-02-08 22:33:03 +0900

    fix: color gradation

--- src/draw_line.c
+++ src/draw_line.c
@@ -1,31 +1,50 @@
 #include "fdf.h"
 
-int	get_color_grad(int color_code0, int color_code1, double step)
+static int	_get_delta_color(int start_color, int end_color, int step)
 {
-	return ((color_code1 - color_code0) / step);
+	int	diff_r;
+	int	diff_g;
+	int	diff_b;
+	int	delta;
+
+	diff_r = (((end_color >> 16) & 0xFF) - ((start_color >> 16) & 0xFF));
+	diff_g = (((end_color >> 8) & 0xFF) - ((start_color >> 8) & 0xFF));
+	diff_b = ((end_color & 0xFF) - (start_color & 0xFF));
+	delta = (diff_r / step) << 16;
+	delta += (diff_g / step) << 8;
+	delta += diff_b / step;
+	return (delta);
 }
 
-void	draw_line(t_image_info *img, t_point *map0,
-	t_point *map1, double **matrix)
+static int	_get_color(int start_color, int delta_color, int step)
+{
+	return (start_color
+		+ ((((delta_color >> 16) & 0xFF) * step) << 16)
+		+ ((((delta_color >> 8) & 0xFF) * step) << 8)
+		+ ((delta_color) & 0xFF) * step);
+}
+
+void	draw_line(t_image_info *img, t_point *start,
+	t_point *end, double **matrix)
 {
 	double	delta_x;
 	double	delta_y;
-	int		step;
+	int		delta_color;
 	double	step_max;
-	int		color_grad;
+	int		step;
 
-	trans_point(map0, map1, matrix);
-	delta_x = map1->x - map0->x;
-	delta_y = map1->y - map0->y;
+	trans_point(start, end, matrix);
+	delta_x = end->x - start->x;
+	delta_y = end->y - start->y;
 	step_max = ft_max(ft_abs(delta_x), ft_abs(delta_y));
 	delta_x /= step_max;
 	delta_y /= step_max;
-	color_grad = get_color_grad(map0->color, map1->color, step_max);
+	delta_color = _get_delta_color(start->color, end->color, (int)step_max);
 	step = 0;
 	while (step < step_max)
 	{
-		my_mlx_pixel_put(img, map0->x + delta_x * step,
-			map0->y + delta_y * step, map0->color + color_grad * step);
-		step++;
+		put_pixel(img, start->x + delta_x * step,
+			start->y + delta_y * step, _get_color(start->color, delta_color, step));
+		step += 1;
 	}
 }
