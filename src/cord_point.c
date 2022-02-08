1692866fe6f67dd0abd155b69f366f215877601b
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-02-09 00:21:07 +0900

    rename

--- src/cord_point.c
+++ src/cord_point.c
@@ -1,6 +1,6 @@
 #include "fdf.h"
 
-void	ft_rotation(double **matrix, double *x, double *y, double *z)
+static void	_trans_with_matrix(double **mtx, double *x, double *y, double *z)
 {
 	double	prev_x;
 	double	prev_y;
@@ -9,22 +9,22 @@ void	ft_rotation(double **matrix, double *x, double *y, double *z)
 	prev_x = *x;
 	prev_y = *y;
 	prev_z = *z;
-	*x = (matrix[0][0] * prev_x
-			+ matrix[0][1] * prev_y
-			+ matrix[0][2] * prev_z
-			+ matrix[0][3]);
-	*y = (matrix[1][0] * prev_x
-			+ matrix[1][1] * prev_y
-			+ matrix[1][2] * prev_z
-			+ matrix[1][3]);
-	*z = (matrix[2][0] * prev_x
-			+ matrix[2][1] * prev_y
-			+ matrix[2][2] * prev_z
-			+ matrix[2][3]);
+	*x = (mtx[0][0] * prev_x
+			+ mtx[0][1] * prev_y
+			+ mtx[0][2] * prev_z
+			+ mtx[0][3]);
+	*y = (mtx[1][0] * prev_x
+			+ mtx[1][1] * prev_y
+			+ mtx[1][2] * prev_z
+			+ mtx[1][3]);
+	*z = (mtx[2][0] * prev_x
+			+ mtx[2][1] * prev_y
+			+ mtx[2][2] * prev_z
+			+ mtx[2][3]);
 }
 
 void	trans_point(t_point *map0, t_point *map1, double **matrix)
 {
-	ft_rotation(matrix, &(map0->x), &(map0->y), &(map0->z));
-	ft_rotation(matrix, &(map1->x), &(map1->y), &(map1->z));
+	_trans_with_matrix(matrix, &(map0->x), &(map0->y), &(map0->z));
+	_trans_with_matrix(matrix, &(map1->x), &(map1->y), &(map1->z));
 }
