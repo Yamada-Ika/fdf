3062efee8ed8ef325f1f959d987d267ab8a62959
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-02-09 18:29:44 +0900

    fix

--- src/display_default_projection.c
+++ src/display_default_projection.c
@@ -2,12 +2,14 @@
 
 bool	can_display_default_projection(t_map_info *map)
 {
-	map->zoom_rate = 255 * pow(map->row_size * map->col_size, -0.507);
-	map->shift_x = WIDTH / 3;
-	map->shift_y = HEIGHT / 3;
-	map->mouse_x = WIDTH / 3;
-	map->mouse_y = HEIGHT / 3;
+	map->zoom_rate = 150 * pow(map->row_size * map->col_size, -0.507);
+	map->mouse_x = 0.0;
+	map->mouse_y = 0.0;
 	if (!can_display_map(map))
+		return (false);
+	map->shift_x = WIDTH / 3;
+	map->shift_y = HEIGHT / 3 + 250;
+	if (!can_redisplay_map(map))
 		return (false);
 	map->yaw = degree_to_radian(-45);
 	if (!can_redisplay_map(map))
